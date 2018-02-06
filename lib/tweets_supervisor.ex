8e2e95a8e42e9bea34fd8cac5bde7b26aaa74ac4
Marco Milanesi <kpanic@gmail.com>
2018-02-06 21:22:34 +0100

    Use DynamicSupervisor

--- lib/tweets_supervisor.ex
+++ lib/tweets_supervisor.ex
@@ -1,25 +1,18 @@
 defmodule Tweetyodel.Worker.Supervisor do
-  use Supervisor
+  use DynamicSupervisor
 
   def start_link do
     # We are now registering our supervisor process with a name
     # so we can reference it in the `start_tweet/1` function
-    Supervisor.start_link(__MODULE__, [], name: :tweet_supervisor)
+    DynamicSupervisor.start_link(__MODULE__, [], name: :tweet_supervisor)
   end
 
   def start_tweet(name) do
-    Supervisor.start_child(:tweet_supervisor, [name])
+    spec = Supervisor.Spec.worker(Tweetyodel.Worker, [name])
+    DynamicSupervisor.start_child(:tweet_supervisor, spec)
   end
 
   def init(_) do
-    children = [
-      worker(Tweetyodel.Worker, [])
-    ]
-
-    # We also changed the `strategy` to `simple_one_for_one`.
-    # With this strategy, we define just a "template" for a child,
-    # no process is started during the Supervisor initialization,
-    # just when we call `start_child/2`
-    supervise(children, strategy: :simple_one_for_one)
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
